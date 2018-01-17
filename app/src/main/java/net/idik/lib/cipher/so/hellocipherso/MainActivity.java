040780b3fab6243ae7811bd3c7e458ad3417cf9d
林帅斌 <me@idik.net>
2018-01-17 14:14:07 +0800

    update to the snapshot version

--- app/src/main/java/net/idik/lib/cipher/so/hellocipherso/MainActivity.java
+++ app/src/main/java/net/idik/lib/cipher/so/hellocipherso/MainActivity.java
@@ -39,6 +39,7 @@ public class MainActivity extends AppCompatActivity {
 
         // Example of a call to a native method
         TextView tv = (TextView) findViewById(R.id.sample_text);
+        String dbKey = CipherClient.数据库密码();
         tv.setText(stringFromJNI() + "\n\n" + CipherClient.hello() + "\n\n" + CipherClient.数据库密码());
     }
 
